e044c43d1682b41baa2b319333a5f1b93044ef24
kvzhao <kelispinor@gmail.com>
2019-01-21 14:47:42 +0800

    Add unit test for base class of evaluation.

--- evaluator/evaluation_base_test.py
+++ evaluator/evaluation_base_test.py
@@ -1,44 +1,101 @@
 
 import numpy as np
-
 from evaluation_base import EmbeddingContainer, AttributeContainer
 
-# Evaluator Builder
-# Customized Evaluator
-# Evaluator Base
+import unittest
+from collections import defaultdict
 
-batch = 32
-embed_size = 1024
+class TestEmbeddingContainer(unittest.TestCase):
+    def test_init(self):
+        container_size = 32
+        embed_size = 1024
+        logit_size = 0
+        embed_container = EmbeddingContainer(embedding_size=embed_size,
+            logit_size=logit_size, container_size=container_size)
 
-features = np.random.rand(batch, embed_size)
+    def test_add_function_and_internals(self):
+        """
+            Push mock data and fetch. Check pushed data and answers are consistent.
+        """
 
-embed_container = EmbeddingContainer(embedding_size=embed_size)
+        batch_size = 512
+        container_size = 1000
+        embed_size = 1024
+        logit_size = 128
+        
+        embedding_container = EmbeddingContainer(embedding_size=embed_size,
+            logit_size=logit_size, container_size=container_size)
+        mock_embeddings = np.random.rand(batch_size, embed_size)
+        mock_logits = np.random.rand(batch_size, logit_size)
 
-for source_id, feat in enumerate(features):
-    embed_container.add(source_id, 0, feat)
+        correct_image_ids = []
+        correct_label_ids = []
+        image_id_to_label_id = {}
+        label_id_to_image_ids = defaultdict(list)
 
-print(embed_container.embeddings)
-print(embed_container.embeddings.shape)
+        for image_id, (embedding, logit) in enumerate(zip(mock_embeddings, mock_logits)):
+            label_id = image_id % 10
+            correct_label_ids.append(label_id)
+            correct_image_ids.append(image_id)
+            image_id_to_label_id[image_id] = label_id
+            label_id_to_image_ids[label_id].append(image_id)
+            embedding_container.add(image_id, label_id, embedding, logit)
 
-print (embed_container.get_embedding_by_image_ids([0,5,6]))
-print (embed_container.get_embedding_by_image_ids([0,5,6]).shape)
+        self.assertEqual(embedding_container.embeddings.shape[0],
+                         embedding_container.logits.shape[0],
+                         'Internal numpy matrix: logit and embedding are not with the same container size.')
+        self.assertTrue(np.allclose(mock_embeddings, embedding_container.embeddings), 'Mismatching embeddings.')
+        self.assertTrue(np.allclose(mock_logits, embedding_container.logits), 'Mismatching logits.')
+        self.assertEqual(batch_size, embedding_container.counts, 'Embeddings not all pushed in container.')
 
-print (embed_container.get_label_by_image_ids([0]))
-print (embed_container.get_label_by_image_ids(0))
+        for label_id, image_ids in label_id_to_image_ids.items():
+            self.assertEqual(image_ids,
+                embedding_container.get_image_ids_by_label(label_id),
+                'Feteched image ids are not equal.')
 
+        for image_id, label_id in image_id_to_label_id.items():
+            returned_label_id = embedding_container.get_label_by_image_ids(image_id)
+            self.assertEqual(returned_label_id, label_id, 'Feteched label ids are not equal.')
 
-attr_container = AttributeContainer()
+        self.assertEqual(correct_image_ids, embedding_container.image_ids,
+                         'Fetched image_ids are not equal to image_ids.')
 
-attr_container.add("color.red", [2, 5, 6, 8, 10])
-attr_container.add("color.blue", [1, 2, 4, 7, 9])
-attr_container.add("color.red", [11, 13, 17])
-attr_container.add("color.blue", 19)
+class TestAttributeContainer(unittest.TestCase):
 
-print (attr_container.groups)
+    def test_add_function_and_internals(self):
+        """
+            Push mock data and fetch. Check pushed data and answers are consistent.
+        """
+        attribute_container = AttributeContainer()
 
+        mock_database = {
+            1: ["Color.Red", "Shape.Bottle", "Pose.isFront"],
+            2: ["Color.Red", "Shape.Can", "Pose.isBack"],
+            3: ["Color.Blue", "Shape.Bottle"],
+        }
+        # prepare targets
+        all_image_ids = list(mock_database.keys())
+        all_attributes = []
+        for _, _attr in mock_database.items():
+            all_attributes.extend(_attr)
+        all_attributes = list(set(all_attributes))
 
-for k, v in attr_container.groups.items():
-    print (k, v)
+        correct_groupings = {
+            'Color.Red': [1, 2],
+            'Color.Blue': [3],
+            'Shape.Can': [2],
+            'Shape.Bottle': [1, 3],
+            'Pose.isFront': [1],
+            'Pose.isBack': [2],
+        }
 
-print (attr_container.attr_lookup)
-print (attr_container.attr_name_lookup)
+        # push and assert
+        for _img_id, _attrs in mock_database.items():
+            attribute_container.add(_img_id, _attrs)
+        
+        self.assertEqual(correct_groupings,
+                         attribute_container.groups,
+                         '')
+
+if __name__ == '__main__':
+    unittest.main()
