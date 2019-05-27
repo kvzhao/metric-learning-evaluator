7d663a2be19dae022115843fbee1fc17e42013b9
kevin.zhao <kevin.zhao@viscovery.com>
2019-05-27 07:56:14 +0000

    Partially re-implementation ranking without attribute container

--- metric_learning_evaluator/evaluations/ranking_evaluation.py
+++ metric_learning_evaluator/evaluations/ranking_evaluation.py
@@ -25,12 +25,11 @@ from metric_learning_evaluator.metrics.ranking_metrics import RankingMetrics
 from metric_learning_evaluator.index.utils import euclidean_distance
 from metric_learning_evaluator.index.utils import indexing_array
 
-from metric_learning_evaluator.index.np_agent import NumpyAgent
-from metric_learning_evaluator.index.hnsw_agent import HNSWAgent
+from metric_learning_evaluator.index.agent import IndexAgent
 
 from metric_learning_evaluator.utils.sample_strategy import SampleStrategyStandardFields as sample_fields
 from metric_learning_evaluator.utils.sample_strategy import SampleStrategy
-from metric_learning_evaluator.query.standard_fields import AttributeStandardFields as attribute_fields
+from metric_learning_evaluator.query.standard_fields import AttributeStandardFields as attr_fields
 
 class RankingEvaluationStandardFields(object):
     # Some keys only used in ranking evaluation
@@ -95,33 +94,101 @@ class RankingEvaluation(MetricEvaluationBase):
                             _metric_names.append(_name)
         return _metric_names
 
-    def new_compute(self, embedding_container, attribute_container=None):
+    def compute(self, embedding_container, attribute_container=None):
         result_container = ResultContainer()
-
         has_database = self.configs.has_database
         # Two types of query attributes
-        print (self.configs.agent_type)
-        instance_ids = embedding_container.instance_ids
-        label_ids = embedding_container.get_label_by_instance_ids(instance_ids)
-        print(len(instance_ids), len(label_ids))
+        agent_type = self.configs.agent_type
 
+        ranking_config = self.metrics
+        sampling_config = self.sampling
 
-        if not(attribute_container is None and has_database):
+        if not (attribute_container is None or not has_database):
             # With attribute container & database
+            print ('Has attribute container')
             print(attribute_container.attribute_names)
             print(attribute_container.instance_to_attribute)
             print(attribute_container.attribute_to_instance)
             print(self.configs.has_database)
             print(self.attributes)
-            print(self.attribute_items)
-        else:
-            # Without attribute container
-            print('No attribute container')
-            print(self.configs.has_database)
-            print(self.attributes)
-            print(self.attribute_items)
 
-    def compute(self, embedding_container, attribute_container=None):
+            # Grouping & Reference are independent
+            
+            # ===== Groupings =====
+            group_items = self.configs.attribute_group_items
+
+            if attr_fields.database in group_items and attr_fields.query in group_items:
+                # database and query are provided
+                pass
+            else:
+                # database and query should be sampled
+                pass
+
+
+            # ====== Cross References =====
+            cref_items = self.configs.attribute_cross_reference_items
+            print(cref_items)
+            # NOTE: How to handle db & query here?
+            
+        else:
+            # Without attribute container, sample database and query for evaluation
+            instance_ids = embedding_container.instance_ids
+            label_ids = embedding_container.label_ids
+
+            sampler = SampleStrategy(instance_ids, label_ids)
+            sampled = sampler.sample_query_and_database(
+                class_sample_method=sampling_config[sample_fields.class_sample_method],
+                instance_sample_method=sampling_config[sample_fields.instance_sample_method],
+                num_of_db_instance_per_class=sampling_config[sample_fields.num_of_db_instance_per_class],
+                num_of_query_class=sampling_config[sample_fields.num_of_query_class],
+                num_of_query_instance_per_class=sampling_config[sample_fields.num_of_query_instance_per_class],
+                maximum_of_sampled_data=sampling_config[sample_fields.maximum_of_sampled_data],)
+
+            sampled_query_instance_ids = sampled[sample_fields.query_instance_ids]
+            sampled_query_label_ids = sampled[sample_fields.query_label_ids]
+            sampled_database_instance_ids = sampled[sample_fields.database_instance_ids]
+            sampled_database_label_ids = sampled[sample_fields.database_label_ids]
+
+            query_embeddings = embedding_container.get_embedding_by_instance_ids(sampled_query_instance_ids)
+            database_embeddings = embedding_container.get_embedding_by_instance_ids(sampled_database_instance_ids)
+
+            sampled_query_instance_ids = np.asarray(sampled_query_instance_ids)
+            sampled_query_label_ids = np.asarray(sampled_query_label_ids)
+            sampled_database_instance_ids = np.asarray(sampled_database_instance_ids)
+            sampled_database_label_ids = np.asarray(sampled_database_label_ids)
+
+            agent = IndexAgent(agent_type, sampled_database_instance_ids, database_embeddings)
+
+            # Search in batch
+            top_k_list = ranking_config[metric_fields.top_k_hit_accuracy]
+            max_k = max(top_k_list)
+
+            # shape (N, max_K)
+            retrieved_database_instance_ids, retrieved_database_distances = agent.search(query_embeddings, top_k=max_k)
+
+            for top_k in top_k_list:
+                if top_k == 1:
+                    continue
+                ranking_metrics = RankingMetrics(top_k)
+                hit_arrays = np.empty((query_embeddings.shape[0], top_k), dtype=np.bool)
+                for _idx, query_label_id in enumerate(sampled_query_label_ids):
+                    retrived_instances = retrieved_database_instance_ids[_idx]
+                    retrived_labels = embedding_container.get_label_by_instance_ids(retrived_instances)
+                    hits = retrived_labels[:top_k] == query_label_id
+                    hit_arrays[_idx, ...] = hits
+
+                ranking_metrics.add_inputs(hit_arrays)
+                result_container.add(attr_fields.All, ranking_fields.top_k_hit_accuracy,
+                                     ranking_metrics.topk_hit_accuracy, condition={'k': top_k})
+            # top k and mAP
+            result_container.add(attr_fields.All, ranking_fields.top_k_hit_accuracy,
+                                 ranking_metrics.top1_hit_accuracy, condition={'k': 1})
+            result_container.add(attr_fields.All, ranking_fields.mAP,
+                                 ranking_metrics.mean_average_precision)
+            return result_container
+
+
+    def old_compute(self, embedding_container, attribute_container=None):
         result_container = ResultContainer()
 
         # Check whether attribute_container is given or not.
@@ -168,6 +235,7 @@ class RankingEvaluation(MetricEvaluationBase):
             top_k_list = ranking_config[metric_fields.top_k_hit_accuracy]
 
             # TODO @kv: check the list and default logic
+            # The following ranking can be module
 
             for top_k in top_k_list:
 
@@ -188,12 +256,12 @@ class RankingEvaluation(MetricEvaluationBase):
                     hit_arrays[_idx, ...] = hits
 
                 ranking_metrics.add_inputs(hit_arrays)
-                result_container.add(attribute_fields.All, ranking_fields.top_k_hit_accuracy,
+                result_container.add(attr_fields.All, ranking_fields.top_k_hit_accuracy,
                                      ranking_metrics.topk_hit_accuracy, condition={'k': top_k})
 
-            result_container.add(attribute_fields.All, ranking_fields.top_k_hit_accuracy,
+            result_container.add(attr_fields.All, ranking_fields.top_k_hit_accuracy,
                                  ranking_metrics.top1_hit_accuracy, condition={'k': 1})
-            result_container.add(attribute_fields.All, ranking_fields.mAP,
+            result_container.add(attr_fields.All, ranking_fields.mAP,
                                  ranking_metrics.mean_average_precision)
 
             return result_container
