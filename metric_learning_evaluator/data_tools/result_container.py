24bb01ae837aa4fc2d0af1ed86200ff8c2b851bd
lotus.sun <lotus.sun@viscovery.com>
2019-07-10 10:49:34 +0000

    Change ResultContainer._result to DataFrame

--- metric_learning_evaluator/data_tools/result_container.py
+++ metric_learning_evaluator/data_tools/result_container.py
@@ -38,7 +38,7 @@ class ResultContainer(object):
                 Generated from ConfigParser.get_attributes()
 
         """
-        self._results = {}
+        self._results = pd.DataFrame()
         # A buffer for storing intermediate results,
         # only show when off-line mode is used.
         self._event_buffer = pd.DataFrame()
@@ -47,23 +47,20 @@ class ResultContainer(object):
         """Add one result
             * create dict if key does not exist
 
-            NOTE: threshold can be None
+            NOTE: threshold can not be None
         """
-        if attribute not in self._results:
-            self._results[attribute] = {}
-        if metric not in self._results[attribute]:
-            self._results[attribute][metric] = {}
-
-        if condition is None:
-            self._results[attribute][metric][''] = value
-        elif isinstance(condition, dict):
-            _cond_key = ''
+        self._results = self._results.append({'attribute': attribute,
+                                              'metric': metric,
+                                              'value': value,
+                                              'condition_name': None,
+                                              'condition_threshold': None}, ignore_index=True)
+        if condition:
             for _cond_name, _threshold in condition.items():
-                _cond_key += '@{}={}'.format(_cond_name, _threshold)
-            self._results[attribute][metric][_cond_key] = value
-        elif isinstance(condition, str):
-            _cond_key = condition
-            self._results[attribute][metric][_cond_key] = value
+                self._results = self._results.append({'attribute': attribute,
+                                                    'metric': metric,
+                                                    'value': value,
+                                                    'condition_name': _cond_name,
+                                                    'condition_threshold': _threshold}, ignore_index=True)
 
     def add_event(self, content):
         """
@@ -96,16 +93,17 @@ class ResultContainer(object):
     @property
     def flatten(self):
         dict_flatten = {}
-        for _attr_name, _metric in self._results.items():
-            for _metric_name, _content in _metric.items():
-                if not _content:
-                    continue
-                for _cond_key, _value in _content.items():
-                    if _cond_key == '':
-                        _name = '{}/{}'.format(_attr_name, _metric_name)
-                    else:
-                        _name = '{}/{}{}'.format(_attr_name, _metric_name, _cond_key)
-                    dict_flatten[_name] = _value
+        for row in self._results.itertuples(index=False):
+            attribute           = row.__getattribute__('attribute')
+            metric              = row.__getattribute__('metric')
+            value               = row.__getattribute__('value')
+            condition_name      = row.__getattribute__('condition_name')
+            condition_threshold = row.__getattribute__('condition_threshold')
+            
+            name = '{}/{}'.format(attribute, metric)
+            if condition_name and condition_threshold:
+                name += '@{}={}'.format(condition_name, condition_threshold)
+            dict_flatten[name] = value
         return dict_flatten
 
     @flatten.setter
