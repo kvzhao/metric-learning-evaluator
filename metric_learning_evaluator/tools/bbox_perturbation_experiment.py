fc951194f0f1b6b848976f9ba563bbba4b5d5780
kevin.zhao <kevin.zhao@viscovery.com>
2019-07-31 08:28:14 +0000

    Add a test func for shift

--- metric_learning_evaluator/tools/bbox_perturbation_experiment.py
+++ metric_learning_evaluator/tools/bbox_perturbation_experiment.py
@@ -6,6 +6,7 @@
 import yaml
 import numpy as np
 
+from tqdm import tdqm
 from scutils.scdata import DatasetBackbone
 
 from metric_learning_evaluator.inference.utils.image_utils import crop_and_resize
@@ -36,6 +37,7 @@ def main(args):
     data_dir = args.data_dir
     out_dir = args.out_dir
     config_path = args.config_path
+    sample_n = args.sample_n
     with open(config_path, 'r') as fp:
         config_dict = yaml.load(fp)
     extractor_setting = config_dict['extractor_settings']
@@ -46,13 +48,17 @@ def main(args):
     src_db = DatasetBackbone(data_dir)
     filenames = src_db.query_all_img_filenames()
 
+    if sample_n is not None:
+        filenames = sorted(filenames)[: sample_n]
+    print('#of images: {}'.foramt(len(filenames)))
+
     feature_extractor = FeatureExtractor(pb_model_path=model_path,
                                          resize=image_size)
 
     container = EmbeddingContainer(embedding_size, 100000, name='perturbation')
 
     instance_counter = 0
-    for filename in filenames:
+    for filename in tqdm(filenames):
         annotations = src_db.query_anno_info_by_filename(filename)
         image_file_path = src_db.query_img_abspath_by_filename(filename)[0]
 
@@ -70,18 +76,26 @@ def main(args):
               add container with attributes
             """
             for case in CASES:
+
                 if case == 'origin':
                     img = crop_and_resize(image, original_bbox, image_size)
                     feat = feature_extractor.extract_feature(img)
                     perturbed_features[case] = feat
+
                 if case == 'shift_left':
-                    pass
+                    bbox = shift_center_by_offset(original_bbox, offset_y=SHIFT_PERTURBATION)
+                    img = crop_and_resize(image, bbox, image_size)
+                    feat = feature_extractor.extract_feature(img)
+                    perturbed_features[case] = feat
+
                 if case == 'shift_right':
                     pass
+
             for case, feature in perturbed_features.items():
                 container.add(instance_id=instance_counter,
                               label_id=label_id,
                               label_name=label_name,
+                              filename=image_file_path,
                               embedding=feature,
                               attributes={
                                   'annotation_id': annotation_id,
@@ -100,6 +114,8 @@ if __name__ == '__main__':
     parser.add_argument('-dd', '--data_dir', type=str, default=None,
                         help='')
     parser.add_argument('-od', '--out_dir', type=str, default=None,
+                        help='')
+    parser.add_argument('--sample_n', type=int, default=None,
                         help='')
     args = parser.parse_args()
     main(args)
