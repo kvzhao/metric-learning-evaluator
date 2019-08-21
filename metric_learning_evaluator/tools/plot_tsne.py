182e36ed771f8c8f598934ef1bb6f2f675b9b851
kevin.zhao <kevin.zhao@viscovery.com>
2019-08-21 09:08:36 +0000

    Add t-SNE

---
+++ metric_learning_evaluator/tools/plot_tsne.py
@@ -0,0 +1,41 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+
+
+from metric_learning_evaluator.analysis.tsne import TSNE
+from metric_learning_evaluator.data_tools.embedding_container import EmbeddingContainer
+
+
+def main(args):
+
+    data_dir = args.data_dir
+    out_dir = args.output_dir
+
+    container = EmbeddingContainer()
+    container.load(data_dir)
+
+    tsne = TSNE(container,
+                n_iter=args.iterations,
+                perplexity=args.perplexity)
+
+    tsne.run()
+    tsne.save_fig(out_dir)
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser('.')
+
+    parser.add_argument('-dd', '--data_dir', type=str, default=None,
+                        help='Path to exported features.')
+    parser.add_argument('-od', '--output_dir', type=str, default=None,
+                        help='Path to exported features.')
+    parser.add_argument('-i', '--iterations', type=int, default=1000,
+                        help='Number of iteration for running t-SNE.')
+    parser.add_argument('-p', '--perplexity', type=int, default=30.0,
+                        help='Number of iteration for running t-SNE.')
+
+    args = parser.parse_args()
+    main(args)
