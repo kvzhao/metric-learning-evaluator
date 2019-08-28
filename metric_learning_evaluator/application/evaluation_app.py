8d346dbf438b9b929cb9e4bdd9a6d92efc782117
kevin.zhao <kevin.zhao@viscovery.com>
2019-08-28 10:21:17 +0000

    Add option for evaluation two containers

--- metric_learning_evaluator/application/evaluation_app.py
+++ metric_learning_evaluator/application/evaluation_app.py
@@ -40,10 +40,10 @@ import numpy as np
 from metric_learning_evaluator.builder import EvaluatorBuilder
 
 from metric_learning_evaluator.data_tools.embedding_container import EmbeddingContainer
+from metric_learning_evaluator.data_tools.embedding_container_merger import EmbeddingContainerMerger
 from metric_learning_evaluator.utils.switcher import switch
 
 # should cooperate add_container
-from metric_learning_evaluator.utils.io_utils import check_instance_id
 from metric_learning_evaluator.utils.report_writer import ReportWriter
 
 from metric_learning_evaluator.core.standard_fields import ApplicationStatusStandardFields as status_fields
@@ -58,25 +58,28 @@ parser = argparse.ArgumentParser('Command-line Metric Learning Evaluation Tool')
 # must-have argument
 parser.add_argument('--config', '-c', type=str, default=None,
                     help='Path to the evaluation configuration with yaml format.')
-# Read data from args or config.
-parser.add_argument('--data_dir', '-dd', type=str, default=None,
-                    help='Path to the source (query) dataset.')
-
-parser.add_argument('--database', '-db', type=str, default=None,
-                    help='Path to the source dataset, with type folder')
 
 parser.add_argument('--data_type', '-dt', type=str, default='embedding_container',
-                    help='Type of the input dataset, Future supports: embedding_container | embedding_db')
+                    help='Type of the input data, supports embedding_container (folder) | embedding_db (.pkl)')
+
+# Read data from args or config.
+parser.add_argument('--data_dir', '-dd', type=str, default=None,
+                    help='Path to the source (query) data folder.')
+
+parser.add_argument('--anchor_database', '-ad', type=str, default=None,
+                    help='Path to the anchor dataset. Default None, use as gallery as given')
 
 parser.add_argument('--out_dir', '-od', type=str, default=None,
-                    help='Path to the output dir for saving report.')
+                    help='Path to the output folder for saving outcomes and report.')
+
 parser.add_argument('--embedding_size', '-es', type=int, default=128,
                     help='Dimension of the given embeddings.')
-# score_size, prob_size
+
 parser.add_argument('--prob_size', '-ps', type=int, default=0,
                     help='Size of the output probability size used in container, set 0 to disable')
 
 parser.add_argument('--verbose', '-v', action='store_true')
+
 APP_SIGNATURE = '[EVAL]'
 
 
@@ -86,24 +89,27 @@ def main():
     data_type = args.data_type
     data_dir = args.data_dir
     out_dir = args.out_dir
-    database_dir = args.database
+    anchor_database_dir = args.anchor_database
 
     status = status_fields.not_determined
 
+    # check input is given
     if not data_dir:
         raise ValueError('data_dir must be assigned!')
 
+    if out_dir is not None:
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
     # argument logic
-    if data_dir and database_dir:
-        status = status_fields.evaluate_query_database
-        raise NotImplementedError('Query to Database function is not implemented yet.')
-    elif data_dir and database_dir is None:
+    if data_dir and anchor_database_dir:
+        status = status_fields.evaluate_query_anchor
+    elif data_dir and anchor_database_dir is None:
         status = status_fields.evaluate_single_container
 
     if not config_path:
-        # TODO @kv: Generate the default config.
+        # TODO: @kv: Generate the default config.
         raise ValueError('evaluation configuration must be assigned!')
-
     try:
         with open(config_path, 'r') as fp:
             config_dict = yaml.load(fp)
@@ -111,48 +117,68 @@ def main():
         raise IOError('Can not load yaml from {}.'.format(config_path))
         # TODO: create default config instead of error.
 
-    # open file
+    # Prepare data container
+    container = None
+    for case in switch(status):
+        print('{} Executes {}'.format(APP_SIGNATURE, status))
+        if case(status_fields.evaluate_single_container):
+            container = EmbeddingContainer(name='single_container')
+            if data_type == 'embedding_container':
+                container.load(data_dir)
+            elif data_type == 'embedding_db':
+                container.load_pkl(data_dir)
+            # end of switch case
+            break
+
+        if case(status_fields.evaluate_query_anchor):
+            """
+              1. Merge two containers
+              2. Add `query->anchor` command in cross_reference
+              3. Change number of database
+            """
+            merger = EmbeddingContainerMerger()
+
+            query_container = EmbeddingContainer(name='query')
+            anchor_container = EmbeddingContainer(name='anchor')
+            # load query
+            if data_type == 'embedding_container':
+                query_container.load(data_dir)
+            elif data_type == 'embedding_db':
+                query_container.load_pkl(data_dir)
+            # load anchor
+            if data_type == 'embedding_container':
+                anchor_container.load(anchor_database_dir)
+            elif data_type == 'embedding_db':
+                anchor_container.load_pkl(anchor_database_dir)
+
+            container = merger.merge(
+                [query_container, anchor_container])
+            # clear buffer
+            query_container.clear()
+            anchor_container.clear()
+            break
+
+    # Build and run evaluation
     evaluator = EvaluatorBuilder(args.embedding_size,
                                  args.prob_size,
                                  config_dict,
                                  mode='offline')
-
-    container = EmbeddingContainer()
-    if data_type == 'embedding_container':
-        container.load(data_dir)
-    elif data_type == 'embedding_db':
-        container.load_pkl(data_dir)
-
     print('evaluator metric names: {}'.format(evaluator.metric_names))
+    print(container)
+    evaluator.add_container(container)
+    evaluator.evaluate()
 
-    for case in switch(status):
-        print('{} Executes {}'.format(APP_SIGNATURE, status))
+    # Show Results
+    for eval_name, result_container in evaluator.results.items():
+        print(eval_name)
+        display_name = display_namemap[eval_name] if eval_name in display_namemap else eval_name
+        reporter = ReportWriter(result_container)
+        overall_report = reporter.overall_report
+        print(overall_report)
+        if out_dir:
+            path = '/'.join([out_dir, 'result_{}'.format(display_name)])
+            result_container.save(path)
 
-        if case(status_fields.evaluate_single_container):
-            # Add datum through loop
-
-            evaluator.add_container(container)
-            all_measures = evaluator.evaluate()
-
-            print('----- evaluation results -----')
-            # remove this
-            for metric_name in evaluator.metric_names:
-                if metric_name in all_measures:
-                    print('{}: {}'.format(metric_name, all_measures[metric_name]))
-
-            if out_dir is not None:
-                if not os.path.exists(out_dir):
-                    os.makedirs(out_dir)
-
-            for eval_name, container in evaluator.results.items():
-                print(eval_name)
-                display_name = display_namemap[eval_name] if eval_name in display_namemap else eval_name
-                reporter = ReportWriter(container)
-                overall_report = reporter.overall_report
-                print(overall_report)
-                if out_dir:
-                    path = '/'.join([out_dir, 'result_{}'.format(display_name)])
-                    container.save(path)
-
-            # end of switch case
-            break
+    if status == status_fields.evaluate_query_anchor and out_dir:
+        path = '/'.join([out_dir, 'merged_container'])
+        container.save(path)
--- metric_learning_evaluator/core/standard_fields.py
+++ metric_learning_evaluator/core/standard_fields.py
@@ -136,7 +136,7 @@ class ApplicationStatusStandardFields:
     not_determined = 'not_determined'
     # evaluation applications
     evaluate_single_container = 'evaluate_single_container'
-    evaluate_query_database = 'evaluate_query_database'
+    evaluate_query_anchor = 'evaluate_query_anchor'
     # inference applications
     inference_feature_extraction = 'inference_feature_extraction'
 
